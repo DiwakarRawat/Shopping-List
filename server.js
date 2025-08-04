7107bceb07f58ca1caef6cc2d4f6d6fda4e5a167
DiwakarRawat <diwakarrawat2003@gmail.com>
2025-08-04 21:32:32 +0530

    Debugging: Temporarily comment out items route

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ mongoose
     .catch(err => console.log(err));
 
 // Use Routes
-//app.use('/api/items', require('./routes/api/items'));
-app.use('/api/users', require('./routes/api/users'));
+app.use('/api/items', require('./routes/api/items'));
+//app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
