f09e87e6bca4cf20ebfcf3623c84c1ee5da43d2e
DiwakarRawat <diwakarrawat2003@gmail.com>
2025-08-06 15:04:11 +0530

    new auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -22,8 +22,8 @@ router.post('/', (req, res) => {
     //Check for existing user
     User.findOne({ email })
         .then(user => {
-            if(!user) return res.status(400).json({ msg: 'User does not exists'});  
-
+            if(!user) return res.status(400).json({ msg: 'User does not exists'}); 
+            
             // Validate password
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
